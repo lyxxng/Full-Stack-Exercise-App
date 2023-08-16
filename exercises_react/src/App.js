3493d2a6afa2c0a4c68997b15f643bed4d35ccae
lyxxng <lisa.shan.young@gmail.com>
2023-08-16 16:05:01 -0700

    Add code

--- exercises_react/src/App.js
+++ exercises_react/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import CreateExercisePage from './pages/CreateExercise';
+import EditExercisePage from './pages/EditExercise';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div className="App-header">
+          <Routes>
+            <Route path="/" element={ <HomePage/> }/>
+            <Route path="/add-exercise" element={ <CreateExercisePage/> }/>
+            <Route path="/edit-exercise" element={ <EditExercisePage/> }/>
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
